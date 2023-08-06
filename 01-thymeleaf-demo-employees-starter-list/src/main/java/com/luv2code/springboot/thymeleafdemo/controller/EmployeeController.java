ac99a5c82fa7c311f0d9fb33ce1cf734d80e664f
rsnf <roberto.sarquis@gmail.com>
2023-08-06 15:54:29 -0300

    initial import

---
+++ 01-thymeleaf-demo-employees-starter-list/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -0,0 +1,32 @@
+package com.luv2code.springboot.thymeleafdemo.controller;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.luv2code.springboot.thymeleafdemo.entity.Employee;
+import com.luv2code.springboot.thymeleafdemo.service.EmployeeService;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    private EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+	this.employeeService = employeeService;
+    }
+
+    @GetMapping("/list")
+    public String listEmployees(Model theModel) {
+
+	List<Employee> theEmployees = employeeService.findAll();
+
+	theModel.addAttribute("employees", theEmployees);
+
+	return "list-employees";
+    }
+}
