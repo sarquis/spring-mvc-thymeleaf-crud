87ed2bda5e9854091e9a59a04b8a0cc62989f56b
rsnf <roberto.sarquis@gmail.com>
2023-08-08 20:29:28 -0300

    Add new employee and update.

--- 01-thymeleaf-demo-employees-starter-list/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ 01-thymeleaf-demo-employees-starter-list/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -5,7 +5,10 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.luv2code.springboot.thymeleafdemo.entity.Employee;
 import com.luv2code.springboot.thymeleafdemo.service.EmployeeService;
@@ -27,6 +30,35 @@ public class EmployeeController {
 
 	theModel.addAttribute("employees", theEmployees);
 
-	return "list-employees";
+	return "employees/list-employees";
     }
+
+    @GetMapping("/showFormForAdd")
+    public String showFormForAdd(Model theModel) {
+
+	Employee theEmployee = new Employee();
+
+	theModel.addAttribute("employee", theEmployee);
+
+	return "employees/employee-form";
+    }
+
+    @GetMapping("/showFormForUpdate")
+    public String showFormForUpdate(@RequestParam("employeeId") int theId, Model theModel) {
+
+	Employee theEmployee = employeeService.findById(theId);
+
+	theModel.addAttribute("employee", theEmployee);
+
+	return "employees/employee-form";
+    }
+
+    @PostMapping("/save")
+    public String saveEmployee(@ModelAttribute("employee") Employee theEmployee) {
+
+	employeeService.save(theEmployee);
+
+	return "redirect:/employees/list";
+    }
+
 }
